cc5dd7dcd0869d68518a507543b2a610f7410c5a
artavistadata-del <artavistadata@gmail.com>
2026-04-28 22:13:21 +0700

    update celery config

--- config/celery_conf.py
+++ config/celery_conf.py
@@ -3,13 +3,11 @@ from celery import Celery
 celery_app = Celery(
     "dashboard_worker",
     broker="redis://localhost:6379/0",
-    backend="redis://localhost:6379/1",
     include=["cleaning.tasks"] 
 )
 
 celery_app.conf.update(
-    task_serializer='json',
-    result_serializer='json',
-    accept_content=['json'],
+    task_ignore_result=True, 
+    task_store_errors_even_if_ignored=True, 
     timezone='Asia/Jakarta',
 )
