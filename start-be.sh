f82957714374d4c72cac4c3266be30045b0bb7f5
inikanipp <pengumpulanlomba@gmail.com>
2026-05-30 08:35:54 +0700

    docs : tambahkan delay 15 detik untuk automatic run

--- start-be.sh
+++ start-be.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd /home/artavista/JTS-PROD/jts-backend
 source venv/bin/activate
+sleep 15
+
 uvicorn src.main:app --host 0.0.0.0 --port 8000 --reload
--- start-worker.sh
+++ start-worker.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd /home/artavista/JTS-PROD/jts-backend
 source venv/bin/activate
+
+sleep 15
+
 celery -A src.workers.celery_app worker --loglevel=info
